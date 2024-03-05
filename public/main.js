3adc800e6581b85547fe91108708888dc361ca5f
Derenas <Se.E.Derenas@baypath.net>
2024-03-05 14:24:11 -0500

    better website look and added darkmode

--- public/main.js
+++ public/main.js
@@ -1,17 +1,7 @@
 
-const aboutMeIMG = document.querySelector('#aboutMeIMG');
-const aboutMeTextHidden = document.querySelector('#aboutMeTextHidden');
-var showExtra = false
-aboutMeIMG.addEventListener('click', () => {
-  if(showExtra){ aboutMeTextHidden.classList.add('d-none'); showExtra = false }
-  else{ aboutMeTextHidden.classList.remove('d-none'); showExtra = true }
-});
-
-
+// controls Name fading in and out
 window.addEventListener('scroll', function(event) {
   const name = document.querySelector('#webName');
-  console.log(`${window.scrollY}`);
-  
   if (window.scrollY > 25) {
     if(name.classList.contains('animate__fadeIn')) { name.classList.remove('animate__animated', 'animate__fadeIn');}
     name.classList.add('animate__animated', 'animate__fadeOut');
@@ -21,3 +11,21 @@ window.addEventListener('scroll', function(event) {
     name.classList.add('animate__animated', 'animate__fadeIn');
   }
  });
+
+//dark / light mode control
+const darkModeBtn = document.querySelector('#darkModeBtn');
+darkModeBtn.addEventListener('click', () => {
+  //switches dark mode buuton to light mode 
+  darkModeBtn.innerHTML == "Dark Mode" ? darkModeBtn.innerHTML = "Light Mode" : darkModeBtn.innerHTML = "Dark Mode"; 
+  darkModeBtn.classList.toggle('dark-mode-button');
+  document.querySelector('body').classList.toggle('dark-mode');
+  
+  document.querySelector('.offcanvas').classList.toggle('dark-mode');
+  document.querySelector('.offcanvas-header').classList.toggle('dark-mode');
+  document.querySelector('#hamburgerMenu').classList.toggle('dark-mode-button');
+  document.querySelector('#navBtn').classList.toggle('dark-mode-button');
+  document.querySelector('#closeNav').classList.toggle('dark-mode-button');
+  
+  document.querySelector('#footer').classList.toggle('dark-mode');
+  
+});
