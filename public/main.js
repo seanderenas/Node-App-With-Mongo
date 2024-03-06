6112dd43b12e42edb5d85a1585b97c485d9a0888
Derenas <Se.E.Derenas@baypath.net>
2024-03-06 09:08:24 -0500

    finished darkmode and fixed main content animating on navbar close

--- public/main.js
+++ public/main.js
@@ -3,20 +3,24 @@
 window.addEventListener('scroll', function(event) {
   const name = document.querySelector('#webName');
   if (window.scrollY > 25) {
-    if(name.classList.contains('animate__fadeIn')) { name.classList.remove('animate__animated', 'animate__fadeIn');}
-    name.classList.add('animate__animated', 'animate__fadeOut');
+    if(name.classList.contains('animate__fadeInLeft')) { name.classList.remove('animate__animated', 'animate__fadeInLeft');}
+    name.classList.add('animate__animated', 'animate__fadeOutLeft');
   } 
   else if (window.scrollY < 25) {
-    if(name.classList.contains('animate__fadeOut')) { name.classList.remove('animate__animated', 'animate__fadeOut');}
-    name.classList.add('animate__animated', 'animate__fadeIn');
+    if(name.classList.contains('animate__fadeOutLeft')) { name.classList.remove('animate__animated', 'animate__fadeOutLeft');}
+    name.classList.add('animate__animated', 'animate__fadeInLeft');
   }
  });
 
 //dark / light mode control
 const darkModeBtn = document.querySelector('#darkModeBtn');
+const links = document.querySelectorAll('.navLink');
 darkModeBtn.addEventListener('click', () => {
   //switches dark mode buuton to light mode 
-  darkModeBtn.innerHTML == "Dark Mode" ? darkModeBtn.innerHTML = "Light Mode" : darkModeBtn.innerHTML = "Dark Mode"; 
+  for(var i=0; i<links.length;i++){
+    links[i].classList.toggle('dark-mode');
+  }
+  darkModeBtn.innerHTML == "<strong>Dark Mode</strong>" ? darkModeBtn.innerHTML = "<strong>Light Mode</strong>" : darkModeBtn.innerHTML = "<strong>Dark Mode</strong>"; 
   darkModeBtn.classList.toggle('dark-mode-button');
   document.querySelector('body').classList.toggle('dark-mode');
   
@@ -27,5 +31,6 @@ darkModeBtn.addEventListener('click', () => {
   document.querySelector('#closeNav').classList.toggle('dark-mode-button');
   
   document.querySelector('#footer').classList.toggle('dark-mode');
-  
+
+
 });
