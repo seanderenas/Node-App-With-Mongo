6fac7635a89bf1e2c41604bcff1b2b90c1c1a887
seanderenas <seanderenas@gmail.com>
2024-04-05 14:04:44 -0400

    Images now get added to MongoDB collection and are linked to portfolio entries, added images page and download page

--- src/server.js
+++ src/server.js
@@ -2,16 +2,25 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const expressLayouts = require('express-ejs-layouts')
-const fileUpload = require('express-fileupload');
+//const fileUpload = require('express-fileupload');
+
+const multer = require("multer")
+const { GridFsStorage } = require("multer-gridfs-storage")
+
+const GridFSBucket = require("mongodb").GridFSBucket
+
 const fs = require('fs');
 const app = express()
 require('dotenv').config()
 
 const favicon = require('serve-favicon');
+const { title } = require('process')
 ObjectID = require('mongodb').ObjectID
 
+const url = process.env.MONGO_KEY
+
 /* ~~~~~~~~ LINK TO DB ~~~~~~~~~~~ */
-MongoClient.connect(process.env.MONGO_KEY)
+MongoClient.connect(url)
   .then(client => {
     const db = client.db('PersonalProjectDB')
     const contactFormCollection = db.collection('contactForm')
@@ -30,10 +39,131 @@ MongoClient.connect(process.env.MONGO_KEY)
     app.use(express.static(__dirname + '/public'))
     app.use('/public/images', express.static(__dirname + '/public/images'));
     app.use(favicon(__dirname + '/public/images/icons/favicon.ico'));
-    app.use(fileUpload());
+    //app.use(fileUpload());
     app.use('/', (req, res, next) => {
       console.log(`${req.method} ${req.path}`)
       next()
+    })
+
+    /* ~~~~~~~~ CREATE BUCKET USED AS STORAGE ~~~~~~~~~~~ */
+    const storage = new GridFsStorage({
+      url,
+      file: (req, file) => {
+        //If it is an image, save to photos bucket
+        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+          console.log('Image uploaded with ');
+          return {
+            bucketName: "photos",
+            filename: `${Date.now()}_${file.originalname}`,
+            portfolioName: ''
+          }
+        } else {
+          //Otherwise save to default bucket
+          return `${Date.now()}_${file.originalname}`
+        }
+      },
+    })
+
+    // Set multer storage engine to the newly created object
+    const upload = multer({ storage })
+
+    app.post("/uploadImages", upload.array('file', 3) , function (req, res, next) 
+    { 
+      const files = req.files
+      const body = req.body
+
+      console.log(body);
+      console.log(files);
+
+      
+
+      const database = client.db("test")
+      const images = database.collection("photos.files")
+
+      for(let i=0; i<files.length; i++){
+        console.log(`File Name: ${files[i].filename}`);
+        images.updateOne({"filename" : files[i].filename }, { "$set": {portfolioTitle: req.body.title} });
+        
+      }
+      
+
+      //res.render('createPortfolio', {menu: 'dev', portfolios: portfolioEntry, cssFileName: "styles.css", title: 'Create Porfolio Entries', descripton: "Enter information in to create more entries into the portfolio database." })
+      res.render('index', {menu: 'public', cssFileName: "styles.css", title: "Sean's Portfolio Website", descripton: "Main page of Sean Derenas's portfolio website, scroll down to learn about me and see my projects." })
+      /*res.render({
+        message: "Uploaded",
+        files: files,
+        body: body,
+      })*/
+      
+      // Respond with the file details
+
+      
+
+    })
+
+    /* GET ALL IMAGES */
+    app.get("/images", async (req, res) => {
+      try {
+        await MongoClient.connect(url)
+    
+        const database = client.db("test")
+        const images = database.collection("photos.files")
+        const cursor = images.find({})
+        const count = await cursor.count()
+        if (count === 0) {
+          return res.status(404).send({
+            message: "Error: No Images found",
+          })
+        }
+    
+        const allImages = []
+    
+        await cursor.forEach(item => {
+          allImages.push(item)
+        })
+    
+        res.render('images', {menu: 'dev', cssFileName: 'styles.css', title: 'All images', descripton: 'desc.', files: allImages })
+      } catch (error) {
+        console.log(error)
+        res.status(500).send({
+          message: "Error Something went wrong",
+          error,
+        })
+      }
+    })
+
+    app.get("/download/:filename", async (req, res) => {
+      try {
+        await MongoClient.connect(url)
+    
+        const database = client.db("test")
+    
+        const imageBucket = new GridFSBucket(database, {
+          bucketName: "photos",
+        })
+    
+        let downloadStream = imageBucket.openDownloadStreamByName(
+          req.params.filename
+        )
+    
+        downloadStream.on("data", function (data) {
+          return res.status(200).write(data)
+        })
+    
+        downloadStream.on("error", function (data) {
+          return res.status(404).send({ error: "Image not found" })
+        })
+    
+        downloadStream.on("end", () => {
+          return res.end()
+        })
+      } catch (error) {
+        console.log(error)
+        res.status(500).send({
+          message: "Error Something went wrong",
+          error,
+        })
+      }
     })
 
     /* ~~~~~~~~ ROUTEs ~~~~~~~~~~~ */
@@ -50,12 +180,41 @@ MongoClient.connect(process.env.MONGO_KEY)
         })
         .catch(error => console.error(error))
     })
-    
-    app.post('/createPortfolio', (req, res) => {
+    app.post('/contact', (req, res) => {
+      contactFormCollection.insertOne(req.body)  
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.get('/contactForms', (req, res) => {
+      db.collection('contactForm').find().toArray()
+        .then(forms => {
+          res.render('contactForms.ejs', {menu: 'dev', contactForms: forms, cssFileName: "styles.css", title: "All contact forms", descripton: "Shows all contact forms sent by users." })
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.get('/portfolio', (req, res) => {
+      res.render('portfolio', {menu: 'public', title: 'Porfolio Page', cssFileName: "styles.css", descripton: "Page containing all my projects i want to show off." })
+    })
+
+    /* ~~~~~~~~ LISTEN ~~~~~~~~~~~ */
+    const isProduction = process.env.NODE_ENV === 'production'
+    const port = isProduction ? 7500 : 3000
+    app.listen(port, function () {
+      console.log(`listening on ${port}`)
+    })
+  })
+  .catch(console.error)
+
+/* old file upload 
+app.post('/createPortfolio', (req, res) => {
       /* used for finding all files in dir (will use later)
       fs.readdirSync(__dirname +'/public/images/icons/').forEach(file => {
         console.log(file);
-      }); */
+      }); 
 
       console.log(req.body);
 
@@ -106,38 +265,4 @@ MongoClient.connect(process.env.MONGO_KEY)
           res.redirect('/createPortfolio')
         })
         .catch(error => console.error(error))
-    })
-
-    app.post('/contact', (req, res) => {
-      contactFormCollection.insertOne(req.body)  
-        .then(result => {
-          res.redirect('/')
-        })
-        .catch(error => console.error(error))
-    })
-
-    app.get('/contactForms', (req, res) => {
-      db.collection('contactForm').find().toArray()
-        .then(forms => {
-          res.render('contactForms.ejs', {menu: 'dev', contactForms: forms, cssFileName: "styles.css", title: "All contact forms", descripton: "Shows all contact forms sent by users." })
-        })
-        .catch(error => console.error(error))
-    })
-
-    app.get('/portfolio', (req, res) => {
-      res.render('portfolio', {menu: 'public', title: 'Porfolio Page', cssFileName: "styles.css", descripton: "Page containing all my projects i want to show off." })
-    })
-
-    app.post('/', (req, res) => {
-      console.log(req.body);
-      res.send('man')
-    })
-
-    /* ~~~~~~~~ LISTEN ~~~~~~~~~~~ */
-    const isProduction = process.env.NODE_ENV === 'production'
-    const port = isProduction ? 7500 : 3000
-    app.listen(port, function () {
-      console.log(`listening on ${port}`)
-    })
-  })
-  .catch(console.error)
+    }) */
