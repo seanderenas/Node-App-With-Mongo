792aa6b1f7c5c8388da5a85bbabd8eb4d2f70874
seanderenas <seanderenas@gmail.com>
2024-04-22 14:13:18 -0400

    Added function for adding all images and portfolios to routes, added logging for all server requests, and now displays portfolios and their images on the main page

--- src/server.js
+++ src/server.js
@@ -2,17 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const expressLayouts = require('express-ejs-layouts')
-//const fileUpload = require('express-fileupload');
-
 const multer = require("multer")
 const { GridFsStorage } = require("multer-gridfs-storage")
-
 const GridFSBucket = require("mongodb").GridFSBucket
-
 const fs = require('fs');
 const app = express()
 require('dotenv').config()
-
 const favicon = require('serve-favicon');
 const { title } = require('process')
 ObjectID = require('mongodb').ObjectID
@@ -26,7 +21,7 @@ MongoClient.connect(url)
     const contactFormCollection = db.collection('contactForm')
     const porfolioEntryCollection = db.collection('porfolioEntry');
     console.log(`Connected to Database`)
-    
+
     /* ~~~~~~~~ SETTINGS ~~~~~~~~~~~ */
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs')
@@ -41,7 +36,16 @@ MongoClient.connect(url)
     app.use(favicon(__dirname + '/public/images/icons/favicon.ico'));
     //app.use(fileUpload());
     app.use('/', (req, res, next) => {
-      console.log(`${req.method} ${req.path}`)
+      let logData = `[ ${new Date().toISOString()} ] [${req.ip}] [${req.method}] [${req.path}] [${req.headers['accept-language']}] [${req.headers['user-agent']}] \n`;
+      //console.log(req.headers)
+      if( !req.path.includes('/download/')){
+        fs.appendFile('logs.txt', logData, function (err) { 
+          if (err) throw err; 
+          console.log('logged data'); 
+        }); 
+      }
+
+  
       next()
     })
 
@@ -50,7 +54,7 @@ MongoClient.connect(url)
       url,
       file: (req, file) => {
         //If it is an image, save to photos bucket
-        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+        if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/webp") {
           console.log('Image uploaded with ');
           return {
             bucketName: "photos",
@@ -67,15 +71,8 @@ MongoClient.connect(url)
     // Set multer storage engine to the newly created object
     const upload = multer({ storage })
 
-    app.post("/uploadImages", upload.array('file', 3) , function (req, res, next) 
-    { 
+    app.post("/uploadImages", upload.array('file', 5) , function (req, res, next) { 
       const files = req.files
-      const body = req.body
-
-      console.log(body);
-      console.log(files);
-
-      
 
       const database = client.db("test")
       const images = database.collection("photos.files")
@@ -85,44 +82,38 @@ MongoClient.connect(url)
         images.updateOne({"filename" : files[i].filename }, { "$set": {portfolioTitle: req.body.title} });
         
       }
-      
-
-      //res.render('createPortfolio', {menu: 'dev', portfolios: portfolioEntry, cssFileName: "styles.css", title: 'Create Porfolio Entries', descripton: "Enter information in to create more entries into the portfolio database." })
-      res.render('index', {menu: 'public', cssFileName: "styles.css", title: "Sean's Portfolio Website", descripton: "Main page of Sean Derenas's portfolio website, scroll down to learn about me and see my projects." })
-      /*res.render({
-        message: "Uploaded",
-        files: files,
-        body: body,
-      })*/
-      
-      // Respond with the file details
-
-      
-
+      //after uploading images enter form data to other collection
+      renderWithImagesAndPortfolios('index', {menu: 'public', cssFileName: "styles.css", title: "Sean's Portfolio Website", descripton: "Main page of Sean Derenas's portfolio website, scroll down to learn about me and see my projects." } , res)
     })
 
-    /* GET ALL IMAGES */
-    app.get("/images", async (req, res) => {
+    /* pass it a file name and json for the res.render but lets you access all images too */
+    async function renderWithImagesAndPortfolios(renderFile, jsonDataForFile = [], res) {
       try {
         await MongoClient.connect(url)
-    
         const database = client.db("test")
         const images = database.collection("photos.files")
         const cursor = images.find({})
-        const count = await cursor.count()
+        const count = await images.countDocuments();
         if (count === 0) {
           return res.status(404).send({
             message: "Error: No Images found",
           })
         }
-    
+        
         const allImages = []
-    
         await cursor.forEach(item => {
           allImages.push(item)
         })
-    
-        res.render('images', {menu: 'dev', cssFileName: 'styles.css', title: 'All images', descripton: 'desc.', files: allImages })
+
+        jsonDataForFile['files'] = allImages;
+        
+        porfolioEntryCollection.find().toArray()
+        .then(portfolioEntry => {
+          jsonDataForFile['portfolios'] = portfolioEntry;
+          res.render(renderFile, jsonDataForFile);
+        })
+        .catch(error => console.error(error))
+
       } catch (error) {
         console.log(error)
         res.status(500).send({
@@ -130,6 +121,11 @@ MongoClient.connect(url)
           error,
         })
       }
+      
+    }
+
+    app.get("/images", async (req, res) => {
+      renderWithImagesAndPortfolios('images', {menu: 'dev', cssFileName: 'styles.css', title: 'All images', descripton: 'desc.' }, res)
     })
 
     app.get("/download/:filename", async (req, res) => {
@@ -168,7 +164,11 @@ MongoClient.connect(url)
 
     /* ~~~~~~~~ ROUTEs ~~~~~~~~~~~ */
     app.get('/', (req, res) => {
-      res.render('index', {menu: 'public', cssFileName: "styles.css", title: "Sean's Portfolio Website", descripton: "Main page of Sean Derenas's portfolio website, scroll down to learn about me and see my projects." })
+      porfolioEntryCollection.find().toArray()
+      .then(portfolioEntry => {
+        renderWithImagesAndPortfolios('index', {menu: 'public', portfolios: portfolioEntry, cssFileName: "styles.css", title: "Sean's Portfolio Website", descripton: "Main page of Sean Derenas's portfolio website, scroll down to learn about me and see my projects." }, res)
+      })
+      .catch(error => console.error(error))
     });
     app.get('/signin', (req, res) => {
       res.render('signin', {menu: 'dev', cssFileName: "styles.css", title: 'Sign In Page', descripton: "Please enter your credentials to sign in." })
@@ -197,7 +197,8 @@ MongoClient.connect(url)
     })
 
     app.get('/portfolio', (req, res) => {
-      res.render('portfolio', {menu: 'public', title: 'Porfolio Page', cssFileName: "styles.css", descripton: "Page containing all my projects i want to show off." })
+      renderWithImagesAndPortfolios('portfolio', {menu: 'public', title: 'Porfolio Page', cssFileName: "styles.css", descripton: "Page containing all my projects i want to show off." } , res)
+    
     })
 
     /* ~~~~~~~~ LISTEN ~~~~~~~~~~~ */
@@ -208,61 +209,3 @@ MongoClient.connect(url)
     })
   })
   .catch(console.error)
-
-/* old file upload 
-app.post('/createPortfolio', (req, res) => {
-      /* used for finding all files in dir (will use later)
-      fs.readdirSync(__dirname +'/public/images/icons/').forEach(file => {
-        console.log(file);
-      }); 
-
-      console.log(req.body);
-
-      if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-      }
-      console.log(req.files.sampleFile);
-
-      let uploadedFiles = req.files.sampleFile;
-      let uploadPath;
-      
-      
-      //make dir with portfolio title name
-      fs.access(`${__dirname}/public/images/portfolio/`, (error) => { 
-        console.log(`Accessed: ${__dirname}/public/images/portfolio/`)
-        // To check if the given directory already exists or not 
-        if (!error) { 
-          // If current directory does not exist then create it 
-          fs.mkdir(`${__dirname}/public/images/portfolio/${req.body.title}`, (error) => { 
-            if (error) { 
-              console.log(error); 
-            } else { 
-              console.log("New Directory created successfully !!"); 
-              uploadFileTofolder();
-            } 
-          }); 
-        } else { 
-          console.log("Given Directory already exists !!"); 
-        } 
-      }); 
-      
-      // Use the mv() method to place the file somewhere on your server
-      async function uploadFileTofolder(){
-        for(let i=0; i<uploadedFiles.length; i++){
-          uploadPath = `${__dirname}/public/images/portfolio/${req.body.title}/${uploadedFiles[i].name}`;
-          console.log(`trying to added '${uploadedFiles[i].name}' to '${uploadPath}'`);
-          let file = uploadedFiles[i];
-          file.mv(uploadPath, function(err) {
-            if (err)
-              return res.status(500).send(err);       
-
-            console.log(`Created folder named: ${req.body.title}`)
-          });
-        }
-      }
-      porfolioEntryCollection.insertOne(req.body)  
-        .then(result => {
-          res.redirect('/createPortfolio')
-        })
-        .catch(error => console.error(error))
-    }) */
